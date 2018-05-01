e8a66955a64b8ef7bbd945176622488e73952b50
Phil Cherner <phcherne@microsoft.com>
2018-05-01 17:01:17 -0400

    add dependencies

---
+++ HoloBot/Assets/BotFramework/Receivers/BotMessageTextMeshReceiver.cs
@@ -0,0 +1,32 @@
+using HoloToolkit.Unity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity3dAzure.WebSockets;
+using UnityEngine;
+
+namespace Unity3dAzure.BotFramework {
+  public sealed class BotMessageTextMeshReceiver : TextMeshReceiver {
+
+    public TextToSpeech MyTTS;
+
+    public override void OnReceivedData(object sender, EventArgs args) {
+      if (args == null) {
+        return;
+      }
+
+      if (args.GetType() != typeof(BotMessageEventArgs)) {
+        return;
+      }
+
+      var myArgs = args as BotMessageEventArgs;
+
+      // only update text from bot
+      if (myArgs.IsBot) {
+        text = myArgs.Text;
+        MyTTS.StartSpeaking(text);
+        needsUpdated = true;
+      }
+    }
+  }
+}
---
+++ HoloBot/Assets/Scripts/WrapTextMesh.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class WrapTextMesh : MonoBehaviour
+{
+    public bool WrapOnStart;
+    public float WrapWidth;
+
+    public void Wrap(float MaxWidth)
+    {
+        TextMesh tm = GetComponent<TextMesh>();
+        if (tm == null)
+        {
+            Debug.LogError("TextMesh component not found.");
+            return;
+        }
+
+        Font f = tm.font;
+        string str = tm.text;
+        int nLastWordInd = 0;
+        int nIter = 0;
+        float fCurWidth = 0.0f;
+        float fCurWordWidth = 0.0f;
+        while (nIter < str.Length)
+        {
+            // get char info
+            char c = str[nIter];
+            CharacterInfo charInfo;
+            if (!f.GetCharacterInfo(c, out charInfo))
+            {
+                Debug.Log(charInfo.advance.ToString("F6"));
+                Debug.LogError("Unrecognized character encountered (" + (int)c + "): " + c);
+                return;
+            }
+
+            if (c == '\n')
+            {
+                nLastWordInd = nIter;
+                fCurWidth = 0.0f;
+            }
+            else
+            {
+                if (c == ' ')
+                {
+                    nLastWordInd = nIter; // replace this character with '/n' if breaking here
+                    fCurWordWidth = 0.0f;
+                }
+
+                fCurWidth += charInfo.width;
+                fCurWordWidth += charInfo.width;
+                if (fCurWidth >= MaxWidth)
+                {
+                    str = str.Remove(nLastWordInd, 1);
+                    str = str.Insert(nLastWordInd, "\n");
+                    fCurWidth = fCurWordWidth;
+                }
+            }
+
+            ++nIter;
+        }
+
+        tm.text = str;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (WrapOnStart)
+            Wrap(WrapWidth);
+    }
+}
