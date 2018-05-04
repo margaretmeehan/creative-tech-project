e8ca5813aa13bdfc94209f54770e8a9d06c6812b
Adina Shanholtz <ashanhol@gmail.com>
2018-05-04 13:25:05 -0400

    got scrollbar to work

---
+++ HoloBot/Assets/Scripts/PopulateScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulateScroll : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject button;
+
+    string[] artists = {"Picasso", "Dali", "Matisse", "Monet", "Michelangelo", "Van Gogh", "Frida Kahlo", "Warhol", "Jackson Pollock", "Georgia O'Keeffe" };
+	
+    void Start () {
+		foreach(var artist in artists)
+        {
+            Debug.Log(artist);
+            GameObject b = Instantiate(button, gameObject.transform.parent);
+            b.GetComponentInChildren<Text>().text = artist;
+        }
+        Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
